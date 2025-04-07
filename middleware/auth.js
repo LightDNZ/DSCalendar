b4c42b20774d6e32c0f168fd8e550709547d1bdc
LightDNZ <bielhenri2008@gmail.com>
2025-04-06 21:38:39 -0300

    corrigindo o middleware novamente

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,9 +1,28 @@
 // middleware/auth.js
-export default defineNuxtRouteMiddleware(async () => {
-  const supabase = useSupabaseClient()
-  const { data } = await supabase.auth.getSession()
+import { createClient } from '@supabase/supabase-js'
+import { parseCookies } from 'h3'
 
-  if (!data.session) {
+export default defineNuxtRouteMiddleware(async (to, from) => {
+  if (process.client) return // Evita duplicidade no client
+
+  const cookies = parseCookies(useRequestEvent())
+  const token = cookies['sb-access-token']
+
+  if (!token) {
     return navigateTo('/login')
   }
+
+  const config = useRuntimeConfig()
+  const supabase = createClient(
+    config.public.supabaseUrl,
+    config.public.supabaseKey
+  )
+
+  const { data, error } = await supabase.auth.getUser(token)
+
+  if (error || !data?.user) {
+    return navigateTo('/login')
+  }
+
+  // aqui da para salvar o usuario para usa dps
 })
